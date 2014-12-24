42f655e12b727f04097a35336da55950e8f9fe08
Alberto G. Jácome <agjacome@gmail.com>
2014-12-24 22:04:59 +0100

    Fix CutProteinsWrapper unmarshalling of enzymes
    Requests to HTTP API have been found unresolved whenever a "digest proteins"
    order was sent. The bug was caused by incorrect JAXB marshalling/unmarshalling
    of the "enzymes" field in the object "CutProteinsWrapper".
    
    It looks like JAXB is unable to unmarshall a set of enzymes when typed as a Set
    interface (Set<Enzyme>) and also cannot parse it as expected (with an upper
    "<enzymes></enzymes>" xml element, and each enzyme being its own xml element
    "<enzyme></enzyme>" inside it) without @XmlElementWrapper and @XmlElements
    annotations. It has been solved by using a LinkedHashSet implementation instead
    of the interface and annotating the field with that mentioned annotations.

--- mahmi-http/src/main/java/es/uvigo/ei/sing/mahmi/http/wrappers/CutProteinsWrapper.java
+++ mahmi-http/src/main/java/es/uvigo/ei/sing/mahmi/http/wrappers/CutProteinsWrapper.java
@@ -1,36 +1,36 @@
 package es.uvigo.ei.sing.mahmi.http.wrappers;
 
-import static java.util.Collections.emptySet;
-
-import java.util.Set;
+import java.util.LinkedHashSet;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Value;
 import es.uvigo.ei.sing.mahmi.common.entities.Enzyme;
 import es.uvigo.ei.sing.mahmi.common.entities.Project;
 import es.uvigo.ei.sing.mahmi.common.utils.annotations.VisibleForJAXB;
 
-@Getter
-@ToString
-@EqualsAndHashCode
 @AllArgsConstructor(staticName = "cutProteins")
 @XmlRootElement(name = "cutProteins") @XmlAccessorType(XmlAccessType.FIELD)
-public final class CutProteinsWrapper {
+@Value public final class CutProteinsWrapper {
 
-    private final Project      project;
-    private final Set<Enzyme>  enzymes;
-    private final int          minSize;
-    private final int          maxSize;
+    private final Project project;
+
+    @XmlElementWrapper(name = "enzymes")
+    @XmlElements({@XmlElement(name = "enzyme", type = Enzyme.class)})
+    private final LinkedHashSet<Enzyme> enzymes;
+
+    private final int minSize;
+    private final int maxSize;
 
     @VisibleForJAXB
     public CutProteinsWrapper() {
-        this(new Project(), emptySet(), 0, Integer.MAX_VALUE);
+        this(new Project(), new LinkedHashSet<>(), 0, Integer.MAX_VALUE);
     }
 
 }
--- mahmi-http/src/main/java/es/uvigo/ei/sing/mahmi/http/wrappers/LoadProjectWrapper.java
+++ mahmi-http/src/main/java/es/uvigo/ei/sing/mahmi/http/wrappers/LoadProjectWrapper.java
@@ -5,18 +5,13 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Value;
 import es.uvigo.ei.sing.mahmi.common.entities.Project;
 import es.uvigo.ei.sing.mahmi.common.utils.annotations.VisibleForJAXB;
 
-@Getter
-@ToString
-@EqualsAndHashCode
 @AllArgsConstructor(staticName = "loadProject")
 @XmlRootElement(name = "loadProject") @XmlAccessorType(XmlAccessType.FIELD)
-public class LoadProjectWrapper {
+@Value public class LoadProjectWrapper {
 
     private final Project project;
     private final String  path;
