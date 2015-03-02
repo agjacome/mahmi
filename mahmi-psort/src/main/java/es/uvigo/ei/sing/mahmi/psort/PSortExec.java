0c27b4367b9947c0224dfa23cbe87d4eb6699b97
mahmi <abmiguez@esei.uvigo.es>
2015-03-02 20:21:07 +0100

    Fix psortb errors

--- mahmi-psort/src/main/java/es/uvigo/ei/sing/mahmi/psort/PSortExec.java
+++ mahmi-psort/src/main/java/es/uvigo/ei/sing/mahmi/psort/PSortExec.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,8 @@ public class PSortExec {
     
     private PSortB psb=new PSortB();
     
-    public void exec(char gram, Path inputPath, Path outputPath){
+    public void exec(char gram, Path inputPath){
+        Path outputPath=Paths.get(inputPath.getParent()+"/psortb.out");
         if(psb.sort(gram, inputPath ,outputPath) == 0){
             List<String> out=new ArrayList<String>();
             List<String> in=new ArrayList<String>();
@@ -27,24 +29,30 @@ public class PSortExec {
             } catch (IOException e) {
                 log.error(e.getMessage());
             }            
-            createSortFile(in, out); 
+
+            createSortFile(in, out, inputPath.getParent());
         }
+        deleteOutFile(outputPath);
     }    
     
-    private void createSortFile(List<String> in, List<String> out){
-        File sortFile = new File("/home/mahmi/Escritorio/sort.txt");
-        if (sortFile.exists()) {
-            sortFile.delete();
-            sortFile = new File("/home/mahmi/Escritorio/sort.txt");
-        }
-        writeSortFile(in, out); 
-        
+    private void deleteOutFile(Path path){
+        File outFile = new File(path.toString());
+        outFile.delete();
     }
     
-    private void writeSortFile(List<String> in, List<String> out){
+    private void createSortFile(List<String> in, List<String> out, Path path){
+        File sortFile = new File(path+"/sort.faa");
+        if (sortFile.exists()) {
+            sortFile.delete();
+            sortFile = new File(path+"/sort.faa");
+        }
+        writeSortFile(in, out, path); 
+    }
+    
+    private void writeSortFile(List<String> in, List<String> out, Path path){
         int currentProtein=1;
         int currentLine=19; 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("/home/mahmi/Escritorio/sort.txt"))) {   
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path+"/sort.faa"))) {   
             while(currentProtein<=out.size()/22){
                 String result=out.get(currentLine-1);
             
